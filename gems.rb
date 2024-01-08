989a1b15c5d7eea131d7b5a8dc2fd7dbe5e20e1b
Dimitri Arrigoni <dimitri@arrigoni.me>
2024-01-08 16:59:57 +0100

    feat(package): add version for inifile + add dev gems

--- gems.rb
+++ gems.rb
@@ -3,15 +3,31 @@
 # ```sh
 # bundle config set --local clean 'true'
 # bundle config set --local path 'vendor/bundle'
-# bundle install
+# bundle install --standalone
 # ```
 source 'https://rubygems.org'
-git_source(:github) { |name| "https://github.com/#{name}.git" }
+
+def github(repo, options = {}, &block)
+  block ||= -> { gem(*[File.basename(repo)].concat([{ github: repo }.merge(options)])) }
+
+  # noinspection RubySuperCallWithoutSuperclassInspection
+  super(repo, options, &block)
+end
 
 group :default do
   gem 'htmlbeautifier', '~> 1.4'
-  gem 'inifile'
+  gem 'inifile', '~> 3.0'
   gem 'rake', '~> 13.0'
   gem 'sassc', '~> 2.4'
   gem 'vendorer', '~> 0.2'
+end
+
+group :development do
+  github 'SwagDevOps/kamaze-project', { branch: 'develop' }
+  gem 'rubocop', '~> 1.3'
+  gem 'sys-proc', '~> 1.1'
+  # repl ---------------------------------
+  gem 'interesting_methods', '~> 0.1'
+  gem 'pry', '~> 0.12'
+  gem 'rb-readline', '~> 0.5'
 end
