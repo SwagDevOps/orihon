a933a4eb1785900a0bcbfb15abb1a953853e42cc
Dimitri Arrigoni <dimitri@arrigoni.me>
2024-01-15 09:55:40 +0100

    feat(services): uses registry concern

--- lib/orihon/services.rb
+++ lib/orihon/services.rb
@@ -3,18 +3,20 @@
 require_relative '../orihon'
 
 module Orihon::Services
-  File.realpath(__FILE__).gsub(/\.rb/, '').then do |path|
+  class << self
+    include(::Orihon::Concerns::Registry)
+  end
+
+  File.realpath(__FILE__).gsub(/\.rb/, '').tap do |path|
     {
       BaseService: :base_service,
       DefaultsProvider: :defaults_provider,
       Info: :info,
       Structurizer: :structurizer,
       Template: :template,
-    }.map do |k, v|
-      autoload(k, "#{path}/#{v}").then { [k, v] }
-    end.then do |h|
+    }.then { register(_1, path: path) }.then do |h|
       self.define_singleton_method(:all) do
-        h.map { |k, v| [v, self.const_get(k)] }.to_h
+        h.map { [_1, self.const_get(_2)] }.to_h
       end
     end
   end
